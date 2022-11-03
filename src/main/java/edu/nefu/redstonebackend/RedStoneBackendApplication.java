e842639fa4f2e3636cf82c2c9823ff6bb4706b36
Yasuomvp <teemovv@outlook.com>
2022-11-03 13:20:50 +0800

    Initial commit

---
+++ src/main/java/edu/nefu/redstonebackend/RedStoneBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.nefu.redstonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedStoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedStoneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/nefu/redstonebackend/RedStoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nefu.redstonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedStoneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
